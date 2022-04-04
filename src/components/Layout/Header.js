0c7c31443bda61c2661e0a289f7302ef347a5547
AleLeonowicz <aleks.leonowicz@gmail.com>
2022-04-04 16:13:44 +0200

    Adding to cart and displaying cart functionalities

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,18 +1,18 @@
-import { Fragment } from "react";
+import { Fragment } from 'react';
 
-import mealsImage from "../../assets/meals.jpg";
-import HeaderCartButton from "./HeaderCartButton";
+import mealsImage from '../../assets/meals.jpg';
+import HeaderCartButton from './HeaderCartButton';
 
-import classes from "./Header.module.css";
+import classes from './Header.module.css';
 
-const Header = (props) => {
+const Header = props => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
-      <div className={classes["main-image"]}>
+      <div className={classes['main-image']}>
         <img src={mealsImage} alt="A table full of delicious food" />
       </div>
     </Fragment>
